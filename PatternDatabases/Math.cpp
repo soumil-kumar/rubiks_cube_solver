ff2da59075ea49a4304fb379a70c8a2f2d9da35e
soumil-kumar <hellosoumil@gmail.com>
2025-06-06 21:12:01 +0530

    first commit

---
+++ PatternDatabases/Math.cpp
@@ -0,0 +1,18 @@
+#include "math.h"
+
+//n!
+uint32_t factorial (uint32_t n){
+    return n <= 1 ? 1 : n * factorial(n - 1);
+}
+
+//nPk
+uint32_t pick(uint32_t n, uint32_t k){
+    return factorial(n) / factorial(n-k);
+}
+
+//nCk
+uint32_t choose(uint32_t n, uint32_t k){
+    return (n < k) ? 0 :factorial(n) / (factorial(n-k)*factorial(k));
+}
+
+
